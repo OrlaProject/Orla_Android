68045cad9e316ac27826652ecff239f9ed2c4122
dlthgml1997 <shfksthgml@naver.com>
2019-09-07 07:44:58 +0900

    add ClickListener

--- app/src/main/java/com/genius/orla/UI/menu/MenuDetailActivity.kt
+++ app/src/main/java/com/genius/orla/UI/menu/MenuDetailActivity.kt
@@ -3,14 +3,21 @@ package com.genius.orla.UI.menu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.genius.orla.R
+import org.jetbrains.anko.toast
 
 class MenuDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_detail)
+
+        setOnBtnClickListener()
     }
 
+    private fun setOnBtnClickListener() {
+
+
+    }
 
 
 }
