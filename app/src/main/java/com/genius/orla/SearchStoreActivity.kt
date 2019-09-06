668addd6d12169f1d6ea2acff50aa86a04eba8f4
dlthgml1997 <shfksthgml@naver.com>
2019-09-06 14:10:55 +0900

    fusedLocationProvider

--- app/src/main/java/com/genius/orla/SearchStoreActivity.kt
+++ app/src/main/java/com/genius/orla/SearchStoreActivity.kt
@@ -3,11 +3,15 @@ package com.genius.orla
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
 
 class SearchStoreActivity : AppCompatActivity() {
 
     val TAG = "SearchStoreActivity"
+    var latitude : Double? = null
+    var longitude : Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +26,25 @@ class SearchStoreActivity : AppCompatActivity() {
     }
 
     private fun requestLocation() {
-        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return
         }
+
+        var fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location == null) {
+                        Log.e(TAG, "location get fail")
+                    } else {
+                        Log.d(TAG, "${location.latitude},${location.longitude}")
+                        latitude = location.latitude
+                        longitude = location.longitude
+                    }
+                }
+                .addOnFailureListener {
+                    Log.e(TAG,"location error ${it.message}")
+                    it.printStackTrace()
+                }
     }
 }
