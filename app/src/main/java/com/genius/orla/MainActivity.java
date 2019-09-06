77c6d3fc1c77c2be254ee19fa82b007e9c45c2de
dlthgml1997 <shfksthgml@naver.com>
2019-09-06 10:57:52 +0900

    git init

---
+++ app/src/main/java/com/genius/orla/MainActivity.java
@@ -0,0 +1,14 @@
+package com.genius.orla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
