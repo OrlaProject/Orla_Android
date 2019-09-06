5549ec57afae67758bf8b18cd41fa0d4de484863
dlthgml1997 <shfksthgml@naver.com>
2019-09-07 05:40:01 +0900

    메인 및 과일 디테일 레이아웃

---
+++ app/src/main/java/com/genius/orla/UI/fruit/FruitDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.genius.orla.UI.fruit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.genius.orla.R
+
+class FruitDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fruit_detail)
+    }
+}
--- app/src/main/java/com/genius/orla/UI/order/CheckOrderActivity.kt
+++ app/src/main/java/com/genius/orla/UI/order/CheckOrderActivity.kt
@@ -1,19 +1,101 @@
 package com.genius.orla.UI.order
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.genius.orla.R
 import kotlinx.android.synthetic.main.activity_check_order.*
+import org.jetbrains.anko.custom.style
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.textColor
+import org.jetbrains.anko.toast
 
 class CheckOrderActivity : AppCompatActivity() {
+
+    var orderStatus: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_order)
 
         setOnBtnClickListener()
+        setStatus(orderStatus)
+    }
 
+    private fun setStatus(orderStatus: Int) {
+        when (orderStatus) {
+            1 -> {
+                img_check_order_act_order_complete.isSelected = true
+                txt_check_order_act_order_complete.setTextColor(Color.parseColor(R.color.lightish_blue.toString()))
+
+                img_check_order_act_making.isSelected = false
+                txt_check_order_act_making.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                img_check_order_act_make_complete.isSelected = false
+                txt_check_order_act_make_complete.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                img_check_order_act_finish.isSelected = false
+                txt_check_order_act_finish.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+
+                //버튼 비활성화
+                btn_check_order_check_material.run {
+                    setTextColor(Color.parseColor("#9b9b9b"))
+                    isEnabled = false
+                    toast("제조가 시작될 때까지 기다려 주세요.")
+                }
+            }
+            2 -> {
+                img_check_order_act_order_complete.isSelected = false
+                txt_check_order_act_order_complete.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+
+                img_check_order_act_making.isSelected = true
+                txt_check_order_act_making.setTextColor(Color.parseColor(R.color.lightish_blue.toString()))
+                img_check_order_act_make_complete.isSelected = false
+                txt_check_order_act_make_complete.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                img_check_order_act_finish.isSelected = false
+                txt_check_order_act_finish.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+
+
+                //버튼 활성화
+                btn_check_order_check_material.run {
+                    setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                    isEnabled = true
+                }
+            }
+            3 -> {
+                img_check_order_act_order_complete.isSelected = false
+                txt_check_order_act_order_complete.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                img_check_order_act_making.isSelected = false
+                txt_check_order_act_making.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+
+                img_check_order_act_make_complete.isSelected = true
+                txt_check_order_act_make_complete.setTextColor(Color.parseColor(R.color.lightish_blue.toString()))
+                img_check_order_act_finish.isSelected = false
+                txt_check_order_act_finish.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+
+                //버튼 활성화
+                btn_check_order_check_material.run {
+                    setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                    isEnabled = true
+                }
+            }
+            4 -> {
+                img_check_order_act_order_complete.isSelected = false
+                txt_check_order_act_order_complete.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                img_check_order_act_making.isSelected = false
+                txt_check_order_act_making.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                img_check_order_act_make_complete.isSelected = false
+                txt_check_order_act_make_complete.setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+
+                img_check_order_act_finish.isSelected = true
+                txt_check_order_act_finish.setTextColor(Color.parseColor(R.color.lightish_blue.toString()))
+
+                //버튼 활성화
+                btn_check_order_check_material.run {
+                    setTextColor(Color.parseColor(R.color.charcoal_grey.toString()))
+                    isEnabled = true
+                }
+            }
+            else -> return
+        }
     }
 
     private fun setOnBtnClickListener() {
