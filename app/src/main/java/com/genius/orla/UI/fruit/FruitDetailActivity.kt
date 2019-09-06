42d9a5cd6390f817c8af1d3d421cfe2f5da20610
choidaye <soar_dy@naver.com>
2019-09-07 07:41:33 +0900

    add QR code

--- app/src/main/java/com/genius/orla/UI/fruit/FruitDetailActivity.kt
+++ app/src/main/java/com/genius/orla/UI/fruit/FruitDetailActivity.kt
@@ -10,11 +10,11 @@ import org.jetbrains.anko.toast
 class FruitDetailActivity : AppCompatActivity() {
 
 
-    var grapeCount : Int = 1
-    var appleCount : Int = 1
-    var tomatoCount : Int = 1
+    var grapeCount: Int = 1
+    var appleCount: Int = 1
+    var tomatoCount: Int = 1
 
-    var totalFruit : Int = 0
+    var totalFruit: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,70 +35,64 @@ class FruitDetailActivity : AppCompatActivity() {
     private fun setOnBtnClickListener() {
         // 포도
         btn_fruit_act_grape_minus.setOnClickListener {
-            if(grapeCount ==1 || grapeCount <1)
-                toast("최소 수량 입니다.")
+            if (grapeCount > 0) {
 
-            txt_fruit_act_grape_count.text = (grapeCount-1).toString()
+                grapeCount -= 1
+                txt_fruit_act_grape_count.text = grapeCount.toString()
 
-            totalFruit += grapeCount-1
-            txt_fruit_act_maximum.text = totalFruit.toString()
+                txt_fruit_act_maximum.text = (appleCount + grapeCount + tomatoCount).toString()
+            }
         }
 
         btn_fruit_act_grape_plus.setOnClickListener {
-            if(totalFruit == 10 || totalFruit>10 )
-                toast("최대 수량 입니다.")
+            if (grapeCount >= 0) {
 
+                grapeCount += 1
+                txt_fruit_act_grape_count.text = (grapeCount).toString()
 
-            txt_fruit_act_grape_count.text = (grapeCount+1).toString()
-
-            totalFruit += grapeCount+1
-            txt_fruit_act_maximum.text = totalFruit.toString()
+                txt_fruit_act_maximum.text = (appleCount + grapeCount + tomatoCount).toString()
+            }
         }
 
         //사과
 
         btn_fruit_act_apple_minus.setOnClickListener {
-            if(appleCount ==1 || appleCount <1)
-                toast("최소 수량 입니다.")
+            if (appleCount > 0) {
 
-            txt_fruit_act_apple_count.text = (appleCount-1).toString()
+                appleCount -= 1
+                txt_fruit_act_apple_count.text = (appleCount).toString()
 
-            totalFruit += appleCount-1
-            txt_fruit_act_maximum.text = totalFruit.toString()
+                txt_fruit_act_maximum.text = (appleCount + grapeCount + tomatoCount).toString()
+            }
         }
 
         btn_fruit_act_apple_plus.setOnClickListener {
-            if(totalFruit == 10 || totalFruit>10 )
-                toast("최대 수량 입니다.")
+            if (appleCount >= 0) {
+                appleCount += 1
+                txt_fruit_act_apple_count.text = (appleCount).toString()
 
-
-            txt_fruit_act_apple_count.text = (appleCount+1).toString()
-
-            totalFruit += appleCount+1
-            txt_fruit_act_maximum.text = totalFruit.toString()
+                txt_fruit_act_maximum.text = (appleCount + grapeCount + tomatoCount).toString()
+            }
         }
 
         //토마토
 
         btn_fruit_act_tomato_minus.setOnClickListener {
-            if(tomatoCount ==1 || tomatoCount <1)
-                toast("최소 수량 입니다.")
+            if (tomatoCount > 0) {
+                tomatoCount -= 1
+                txt_fruit_act_tomato_count.text = (tomatoCount).toString()
 
-            txt_fruit_act_tomato_count.text = (tomatoCount-1).toString()
-
-            totalFruit += tomatoCount-1
-            txt_fruit_act_maximum.text = totalFruit.toString()
+                txt_fruit_act_maximum.text = (appleCount + grapeCount + tomatoCount).toString()
+            }
         }
 
         btn_fruit_act_tomato_plus.setOnClickListener {
-            if(totalFruit == 10 || totalFruit>10 )
-                toast("최대 수량 입니다.")
+            if (tomatoCount >= 0) {
+                tomatoCount += 1
+                txt_fruit_act_tomato_count.text = (tomatoCount).toString()
 
-
-            txt_fruit_act_tomato_count.text = (tomatoCount+1).toString()
-
-            totalFruit += tomatoCount+1
-            txt_fruit_act_maximum.text = totalFruit.toString()
+                txt_fruit_act_maximum.text = (appleCount + grapeCount + tomatoCount).toString()
+            }
         }
     }
 }
