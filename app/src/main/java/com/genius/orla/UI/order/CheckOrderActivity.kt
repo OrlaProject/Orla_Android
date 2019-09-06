18e04d90d3c2f6f631c30a7c870afa7df09fae60
dlthgml1997 <shfksthgml@naver.com>
2019-09-07 00:10:27 +0900

    <OrderMaterialActivity> 레이아웃

--- app/src/main/java/com/genius/orla/UI/order/CheckOrderActivity.kt
+++ app/src/main/java/com/genius/orla/UI/order/CheckOrderActivity.kt
@@ -3,11 +3,22 @@ package com.genius.orla.UI.order
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.genius.orla.R
+import kotlinx.android.synthetic.main.activity_check_order.*
+import org.jetbrains.anko.startActivity
 
 class CheckOrderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_order)
+
+        setOnBtnClickListener()
+
+    }
+
+    private fun setOnBtnClickListener() {
+        btn_check_order_check_material.setOnClickListener {
+            startActivity<OrderMaterialActivity>()
+        }
     }
 }
---
+++ app/src/main/java/com/genius/orla/UI/order/OrderMaterialActivity.kt
@@ -0,0 +1,13 @@
+package com.genius.orla.UI.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.genius.orla.R
+
+class OrderMaterialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_material)
+    }
+}
